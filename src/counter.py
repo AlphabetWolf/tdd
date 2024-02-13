f451d26a6a16be2fa28f5debc69d6e61c55331f7
Shijie Lin <1369862453@qq.com>
2024-02-13 14:38:29 -0800

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -1,15 +1,10 @@
 from flask import Flask
 # we need to import the file that contains the status codes
-from src import status 
+from src import status
 app = Flask(__name__)
-
 
 COUNTERS = {}
 
-# We will use the app decorator and create a route called slash counters.
-# specify the variable in route <name>
-# let Flask know that the only methods that is allowed to called
-# on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
@@ -18,10 +13,11 @@ def create_counter(name):
 
     if name in COUNTERS:
         COUNTERS.clear()
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
 
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
@@ -33,6 +29,7 @@ def update_counter(name):
         COUNTERS[name] += 1
         return {name: COUNTERS[name]}, status.HTTP_200_OK
 
+
 @app.route('/counters/<name>', methods=['GET'])
 def read_counter(name):
     """Read a counter"""
@@ -41,7 +38,8 @@ def read_counter(name):
 
     if name in COUNTERS:
         return {name: COUNTERS[name]}, status.HTTP_200_OK
-    
+
+
 @app.route('/counters/<name>', methods=['DELETE'])
 def delete_counter(name):
     """Delete a counter"""
